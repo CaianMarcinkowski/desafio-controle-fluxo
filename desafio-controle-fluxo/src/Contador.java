582e878f2e38b5eeb2aaedb274154dfb573b4fa2
CaianMarcinkowski <caianferre@gmail.com>
2023-08-20 21:46:21 -0300

    Desafio Controle de Fluxo concluido

--- desafio-controle-fluxo/src/Contador.java
+++ desafio-controle-fluxo/src/Contador.java
@@ -1,25 +1,28 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Contador {
     public static void main(String[] args) throws Exception {
 
-        Scanner terminal = new Scanner(System.in);
+        try{
+            Scanner terminal = new Scanner(System.in);
 		
-        System.out.println("Digite o primeiro parâmetro");
-		int parametroUm = terminal.nextInt();
-		System.out.println("Digite o segundo parâmetro");
-		int parametroDois = terminal.nextInt();
+            System.out.println("Digite o primeiro parâmetro");
+		    int parametroUm = terminal.nextInt();
+		    System.out.println("Digite o segundo parâmetro");
+		    int parametroDois = terminal.nextInt();
 		
-		try {
-			//chamando o método contendo a lógica de contagem
-			contar(parametroUm, parametroDois);
-		
+		    //chamando o método contendo a lógica de contagem
+		    contar(parametroUm, parametroDois);
+
+		    terminal.close();
 		}catch (ParametrosInvalidosException exception) {
 			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
             System.err.println(exception.getMessage());
-		}
-
-        terminal.close();
+		} catch (InputMismatchException exception){
+            //imprimir a mensagem: Apenas valores numéricos e inteiros são validos
+            System.err.println("Apenas valores numéricos e inteiros são validos");
+        }
     }
 
     static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
@@ -31,7 +34,7 @@ public class Contador {
 		    int contagem = parametroDois - parametroUm;
 		    //realizar o for para imprimir os números com base na variável contagem
           
-            for (int i = 0; i < contagem; i++) {
+            for (int i = 0; i <= contagem; i++) {
                 System.out.println(parametroUm + i);
             }
         }
